6f79ad09923a21477b3a1cf5667f6f240c8b66e2
Veronika <veronika.lopatiuk@gmail.com>
2022-02-20 13:55:32 +0000

    add program logic

---
+++ server/Components/TrafficController.js
@@ -0,0 +1,89 @@
+const TrafficLight = require("./Data");
+ 
+class Channel {
+    #name;
+    #trafficLights;
+  
+    constructor(name, trafficLights) {
+      if(typeof name !== "string" || name === "") {
+        throw Error("Name can't be an empty string!");
+      }
+  
+      if(!Array.isArray(trafficLights)) {
+        throw Error("Traffic lights must be an array!");
+      }
+      
+      this.#name = name;
+      this.#trafficLights = trafficLights;
+    }
+  
+    get name() {
+      return this.#name;
+    }
+  
+    get trafficLights() {
+      return this.#trafficLights;
+    }
+  
+}
+  
+class Program {
+    #length;
+    #cycle;
+  
+    constructor(length, cycle) {
+      this.#length = length;
+      this.#cycle = cycle;
+    }
+  
+    get length() {
+      return this.#length;
+    }
+  
+    get cycle() {
+      return this.#cycle;
+    }
+}
+
+class Controller {
+  #config;
+
+  constructor() {
+      this.channels = new Map();
+      this.program = null;
+      this.#config = require("../config.json");
+      this.loadChannels();
+      this.loadProgram();
+  }
+
+  update(message) {
+      const now = new Date();
+      const dateTimeFormatted = `${now.toLocaleDateString("fr-CA")} ${now.toLocaleTimeString("fr-FR")}`
+      console.log(`${dateTimeFormatted}: ${message}`);
+  }
+
+  loadChannels() {
+    for (const [key, value] of Object.entries(this.#config.channels)) {
+      const {name, trafficLights} = value;
+      this.channels.set(key, new Channel(name, trafficLights))
+    }
+  }
+
+  loadProgram() {
+    const length = this.#config.program.length;
+    const cycle = new Map();
+
+    for (const [key, value] of Object.entries(this.#config.program.cycle)) {
+      const trafficLights = [];
+      for(let i = 0; i < length; i++) {
+        trafficLights.push(new TrafficLight(value.R[i], value.Y[i], value.G[i]));
+      }
+      cycle.set(key, trafficLights);
+    }
+
+    this.program = new Program(length, cycle);
+  }
+
+}
+
+module.exports = { Controller, Program, Channel };
---
+++ server/main.js
@@ -0,0 +1,24 @@
+const {Controller} = require("./Components/TrafficController");
+const Timer = require("./Components/Timer");
+
+const controller = new Controller();
+
+const client = new Controller();
+const subject = new Timer();
+subject.addObserver(client);
+
+(async () => {
+  try {
+    const message = "started running...";
+    const now = new Date();
+    const dateTimeFormatted = `${now.toLocaleDateString("fr-CA")} ${now.toLocaleTimeString("fr-FR")}`
+    console.log(`${dateTimeFormatted}: ${message}`);
+
+    await subject.startCountDown(5);
+    await subject.startTimer(1);
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+// https://stackoverflow.com/questions/27939773/tolocaledatestring-short-format
