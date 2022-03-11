d795ab66a8573eaff392c64ee186152413550d39
Pavel <pavel.filkovskiy@gmail.com>
2022-03-11 16:23:38 +0000

    add controller logic to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ const SERVER_PORT = process.env.PORT || 4000;
 const express = require("express");
 const cors = require("cors");
 
+const { Controller } = require("./Components/TrafficController");
+const Timer = require("./Components/Timer");
+const { Status } = require("./Components/Model");
+
+const controller = new Controller();
+const timer = new Timer();
+timer.addObserver(controller);
+
 const app = express();
 
 app.use(express.json());
@@ -12,3 +20,13 @@ app.get("/", function (request, response){
 })
 
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`));
+
+(async () => {
+    try {  
+      await timer.startCountDown(5);
+      controller.status = Status.STARTED;
+      await timer.startTimer(1);
+    } catch (error) {
+      console.error(error);
+    }
+})();
