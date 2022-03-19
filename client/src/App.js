6c7f059410317f6396f54b7f059028ecbfdf03f8
Veronika <veronika.lopatiuk@gmail.com>
2022-03-19 10:44:44 +0000

    complete project

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,20 @@ import Road from "./Components/Road";
 import TrafficLight from './Components/TrafficLight';
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState(null);
 
   function handleData(state) {
     setData(state);
   }
 
   useEffect(() => {
-    setInterval(() => {
+    const timerId = setInterval(() => {
       
       fetch("http://localhost:4000/state")
         .then(res => {
+          if(!res.ok){
+            throw Error("Could not fetch the data for that resource")
+          }
           return res.json();
         })
         .then(state => {
@@ -24,26 +27,29 @@ function App() {
         });
 
     }, 1000);
+
+      return (() => clearInterval(timerId));
+
   }, []);
 
   return (
     <div className = "container">
       <Grass>
-        <TrafficLight direction="W" state={data.state.T1} />
+        { !!data && <TrafficLight direction="W" state={data.state.T1} /> }
       </Grass>
       <Road roadType="vertical" />
       <Grass>
-        <TrafficLight direction="N" state={data.state.T2} />
+        { !!data && <TrafficLight direction="N" state={data.state.T2} /> }
       </Grass>
       <Road roadType="horizontal" />
       <Intersection />
       <Road roadType="horizontal" />
       <Grass>
-        <TrafficLight direction="S" state={data.state.T3} />
+        { !!data && <TrafficLight direction="S" state={data.state.T4} /> }
       </Grass>
       <Road roadType="vertical" />
       <Grass>
-        <TrafficLight direction="E" state={data.state.T4} />
+        { !!data && <TrafficLight direction="E" state={data.state.T3} /> }
       </Grass>
     </div>
   );
--- client/src/Components/TrafficLight.js
+++ client/src/Components/TrafficLight.js
@@ -2,9 +2,9 @@ const TrafficLight = ({direction, state}) => {
     return (
         <div className= {`traffic-light-container ${direction}`}>
             <div className={`traffic-light ${direction}`} >
-                <div className = "green"></div>
-                <div className = "yellow"></div>
-                <div className = "red"></div>
+                <div className = {state.G ? "green" : "grey"}></div>
+                <div className = {state.Y ? "yellow" : "grey"}></div>
+                <div className = {state.R ? "red" : "grey"}></div>
 
             </div>
         </div>
